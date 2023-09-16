09bf4bc3c1bd2e22048491e2ca231d9b212ff55a
tradulescu <tristan-mihai.radulescu@etu.umontpellier.fr>
2023-09-16 19:38:37 +0200

    fix AlertModal for build

--- src/component/AlertModal.tsx
+++ src/component/AlertModal.tsx
@@ -10,7 +10,7 @@ type animation_type = "appear" | "disappear";
 export default function AlertModal(props: AlertProps){
   const [animation, setAnimation] = useState<animation_type>("appear")
    return(
-        <div key={props.alert.message} className={`alert-${props.alert.type}`}>
+        <div key={props.alert.message} className={`alert-${props.alert.type} ` + animation}>
           <p>{props.alert.message}</p>
         </div>
    )
